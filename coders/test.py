5987065638e6077cc7994a8e8b3220737e1752cc
harylax <haryandr23@gmail.com>
2026-08-26 03:28:35 +0300

    update

--- coders/test.py
+++ coders/test.py
@@ -1,8 +1,20 @@
+import sys
+
+
 def get_burnout_edge(path: str) -> int:
     content: list[str] = []
-    with open(path) as f:
-        content = f.readlines()
-    new_content: list[str] = [line for line in content if line.strip().endswith('is compiling')]
+    try:
+        with open(path) as f:
+            content = f.readlines()
+    except OSError as err:
+        print(err)
+        sys.exit(1)
+    for ln in content:
+        if ln.strip().endswith('burned out'):
+            return (-1)
+    new_content: list[str] = [
+        line for line in content if line.strip().endswith('is compiling')
+        ]
     data: dict[int, list[int]] = {}
     for line in new_content:
         parts: list[str] = line.split()
@@ -13,8 +25,14 @@ def get_burnout_edge(path: str) -> int:
     for v in data.values():
         for i in range(1, len(v)):
             diff_compiles.append(v[i] - v[i - 1])
+    if not diff_compiles:
+        return (0)
     return max(diff_compiles)
 
 
 if __name__ == "__main__":
-    print(f"Edge burnout: {get_burnout_edge('test.txt')}")
+    edge: int = get_burnout_edge(sys.argv[1])
+    if edge < 0:
+        print("Codexion has burned out")
+    else:
+        print(f"Edge burnout: {edge}")
